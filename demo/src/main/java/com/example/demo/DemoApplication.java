a501e03a1f03ca55aaacdbcd5473a8d4f977ab21
KTH <pni2396@gmail.com>
2026-01-14 23:40:05 +0900

    hotfix: Swagger 테스트 서버 설정

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -5,11 +5,20 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @OpenAPIDefinition(
-        info = @Info(title = "DeepFlow-WebIDE API", description = "DeepFlow 팀-WebIDE 프로젝트 API 명세서", version = "v1")
+        info = @Info(
+                title = "DeepFlow-WebIDE API",
+                description = "DeepFlow 팀-WebIDE 프로젝트 API 명세서",
+                version = "v1"
+        ),
+        servers = {
+                @Server(url = "https://t2.mobidic.shop", description = "배포 서버"),
+                @Server(url = "http://localhost:8080", description = "로컬 개발 서버")
+        }
 )
 @SecurityScheme(
         name = "Authorization",
